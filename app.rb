28a1b1f2578a555b10ccad4a6957bed6485b6891
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-05 23:59:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+  "My name is Tomas"
+end
+
+get '/hometown' do
+  "My hometown is New York"
+end
+
+get '/favorite-song' do
+  "My favorite song is Purple Haze"
+end
+
 end
