7b581d12f928676c5996706718bbfd14dfc44116
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-05 23:55:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+  "Hello, World!"
+end
+
+get '/hometown' do
+  "Hello, World!"
+end
+
+get '/favorite-song' do
+  "Hello, World!"
+end
+
 end
